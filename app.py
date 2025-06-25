7494e4c2d39c8544ee92d97fa274f664a0917f65
Hanna Samuel <hanna17st@gmail.com>
2025-06-25 19:37:53 +0300

    add requirements file

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import streamlit as st
+from PIL import Image
 from ultralytics import YOLO
-import cv2
 import numpy as np
+import tempfile
+import os
 
-st.title("Waste Sorting Detection")
-uploaded_file = st.file_uploader("Upload conveyor image", type=["jpg", "png"])
-if uploaded_file:
-    model = YOLO('runs/detect/taco_yolo8m/weights/best.pt')
-    image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
-    results = model.predict(image)
-    st.image(results[0].plot(), caption="Detected Waste", use_column_width=True)
+# Load the trained model
+model = YOLO("notebooks/yolov8m.pt")
+
+st.set_page_config(page_title="TACO Litter Detection", layout="centered")
+st.title("🗑️ TACO Waste Detection")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image",use_container_width=True)
+
+    if st.button("Run Detection"):
+        with st.spinner("Detecting..."):
+            # Convert PIL to NumPy
+            image_np = np.array(image)
+
+            # Run inference
+            results = model.predict(source=image_np, conf=0.25)
+
+            # Get result image
+            result_img = results[0].plot()
+
+            st.image(result_img, caption="Detection Results", use_column_width=True)
